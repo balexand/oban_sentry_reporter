6f8830b19a433cb6ed4861b37f705947173ac952
Brian Alexander <balexand@gmail.com>
2023-01-16 11:34:36 -0700

    first commit

---
+++ lib/oban_sentry_reporter.ex
@@ -0,0 +1,18 @@
+defmodule ObanSentryReporter do
+  @moduledoc """
+  Documentation for `ObanSentryReporter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ObanSentryReporter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oban_sentry_reporter_test.exs
@@ -0,0 +1,8 @@
+defmodule ObanSentryReporterTest do
+  use ExUnit.Case
+  doctest ObanSentryReporter
+
+  test "greets the world" do
+    assert ObanSentryReporter.hello() == :world
+  end
+end
